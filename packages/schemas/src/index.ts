06752ef93ae82f612bef529e3b38a6c196973dc6
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-02-15 22:14:58 +0530

    register schema created

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -4,5 +4,12 @@ export const registerSchema = z.object({
   email: z.string().email("Invalid email format"),
   password: z.string().min(1, "Password must be atleast 8 characters long"),
   companyName: z.string().min(1, "Organisation must be provided"),
-  address: z.string().optional(),
+  address: z.string().nullable(),
+  city: z.string().nullable(),
+  vatNumber: z.string().min(1, "VAT number is needed to register the organisation"),
+  authorizedRepresentative: z.string().min(1, "Authroized Represntative is needed to register the organisation"),
+  firstName: z.string().min(1, "Please provide you first name"),
+  lastName: z.string().nullable(),
 });
+
+export type RegisterPayload = z.infer<typeof registerSchema>
