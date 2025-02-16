caf889b3a642132fcfef1b7c3568fd08d5cc3b5d
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-02-16 14:21:51 +0530

    minor config for build express server

--- apps/http-server/src/controllers/employeeControllers.ts
+++ apps/http-server/src/controllers/employeeControllers.ts
@@ -1,7 +1,14 @@
 import { Employee } from "@repo/db";
 import { Request, Response } from "express";
+import { registerSchema } from "@repo/schemas";
 
 export const register = async (
   req: Request<unknown, unknown, Employee>,
   res: Response
-) => {};
+) => {
+  const payload = req.body;
+  const parsedResults = registerSchema.safeParse(payload);
+  if (parsedResults.success) {
+    res.send(parsedResults.data);
+  }
+};
