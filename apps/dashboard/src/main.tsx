18b4f98f8f04915c7700926eb467e2550d1150d5
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-02-27 21:23:08 +0530

    added theme

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -1,15 +1,22 @@
 import { Routers } from "./routes";
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider, PasswordInput, TextInput, createTheme, rem } from "@mantine/core";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ContextProvider } from "./context";
+import "@fontsource/red-hat-display/300.css";
+import "@fontsource/red-hat-display/400.css";
+import "@fontsource/red-hat-display/500.css";
+import "@fontsource/red-hat-display/600.css";
+import "@fontsource/red-hat-display/700.css";
+import "@fontsource/red-hat-display/800.css";
+import "@fontsource/red-hat-display/900.css";
 
 const queryClient = new QueryClient();
 
 const MantineTheme = createTheme({
-  // fontFamily: "Space Grotesk, sans-serif",
+  fontFamily: "Red Hat Display, Space Grotesk, sans-serif",
   primaryColor: "dark",
   colors: {
     dark: [
@@ -18,13 +25,45 @@ const MantineTheme = createTheme({
       "#b4b8c7", // 300
       "#8d91a7", // 400
       "#666a88", // 500
-      "#666a88", // Main shade (600)
-      "#1d2031", // 700
-      "#131624", // 800
+      "#666a88",
+      "#212B36", // 700 (Primary shage)
+      "#3c4a59", // 800 (Primay shade when hover)
       "#0e101d", // 900
       "#090a14", // Darkest (1000)
     ],
   },
+
+  headings: {
+    sizes: {
+      h1: { fontSize: rem(20), fontWeight: "700" },
+      h2: { fontSize: rem(30), fontWeight: "700" },
+      h3: { fontSize: rem(40), fontWeight: "700" },
+      h4: { fontSize: rem(50), fontWeight: "700" },
+      h5: { fontSize: rem(60), fontWeight: "700" },
+      h6: { fontSize: rem(60), fontWeight: "700" },
+    },
+  },
+
+  defaultRadius: "md",
+
+  components: {
+    TextInput: TextInput.extend({
+      styles: {
+        input: {
+          padding: "25px",
+          borderColor: "#E8ECEE"
+        }
+      },
+    }),
+    PasswordInput: PasswordInput.extend({
+      styles: {
+        input: {
+          padding: "25px",
+          borderColor: "#E8ECEE"
+        }
+      },
+    })
+  }
 });
 
 // Ref: https://mantine.dev/theming/theme-object/#store-theme-override-object-in-a-variable
--- apps/dashboard/src/pages/Signup/index.tsx
+++ apps/dashboard/src/pages/Signup/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
+import { Button, Center, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
 import { RegisterEmployeePayload, RegisterEmployeeSchema } from "@repo/schemas";
 import { RegisterFormLabels, RegisterFormNames, RegisterFormPlaceholder } from "./utils";
@@ -28,59 +28,49 @@ export function SignUp() {
     registerUser(payload);
   };
   return (
-    <Grid h="100vh">
-      <Grid.Col visibleFrom="sm" span={{ sm: 6 }}>
-        Banner
-      </Grid.Col>
-      <Grid.Col
-        span={{ base: 12, sm: 6 }}
-        p={"md"}
-        h="100vh"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Stack w={{ base: 400, sm: 500 }}>
-          <Stack>
-            <Title>Welcome to clocki!</Title>
-            <Text>Enter your details to create an account</Text>
-          </Stack>
-          <form onSubmit={onSubmit(handlesubmit)}>
-            <Stack>
-              <TextInput
-                {...getInputProps(RegisterFormNames.firstName)}
-                key={key(RegisterFormNames.firstName)}
-                label={RegisterFormLabels.firstName}
-                placeholder={RegisterFormPlaceholder.firstName}
-              />
-              <TextInput
-                {...getInputProps(RegisterFormNames.lastName)}
-                key={key(RegisterFormNames.lastName)}
-                label={RegisterFormLabels.lastName}
-                placeholder={RegisterFormPlaceholder.lastName}
-              />
-
-              <TextInput
-                {...getInputProps(RegisterFormNames.email)}
-                key={key(RegisterFormNames.email)}
-                label={RegisterFormLabels.email}
-                placeholder={RegisterFormPlaceholder.email}
-              />
-
-              <PasswordInput
-                {...getInputProps(RegisterFormNames.password)}
-                key={key(RegisterFormNames.password)}
-                label={RegisterFormLabels.password}
-                placeholder={RegisterFormPlaceholder.password}
-              />
-
-              <Button type="submit">Sign up</Button>
-            </Stack>
-          </form>
+    <Center h="100vh" p="md">
+      <Stack w={{ base: 400, sm: 400 }}>
+        <Stack>
+          <Title size="h6" ta="center"> 
+            Clocki
+          </Title>
+          <Text size="lg" ta="center">
+            Enter your details to create an account
+          </Text>
         </Stack>
-      </Grid.Col>
-    </Grid>
+        <form onSubmit={onSubmit(handlesubmit)}>
+          <Stack>
+            <TextInput
+              {...getInputProps(RegisterFormNames.firstName)}
+              key={key(RegisterFormNames.firstName)}
+              label={RegisterFormLabels.firstName}
+              placeholder={RegisterFormPlaceholder.firstName}
+            />
+            <TextInput
+              {...getInputProps(RegisterFormNames.lastName)}
+              key={key(RegisterFormNames.lastName)}
+              label={RegisterFormLabels.lastName}
+              placeholder={RegisterFormPlaceholder.lastName}
+            />
+
+            <TextInput
+              {...getInputProps(RegisterFormNames.email)}
+              key={key(RegisterFormNames.email)}
+              label={RegisterFormLabels.email}
+              placeholder={RegisterFormPlaceholder.email}
+            />
+
+            <PasswordInput
+              {...getInputProps(RegisterFormNames.password)}
+              key={key(RegisterFormNames.password)}
+              label={RegisterFormLabels.password}
+              placeholder={RegisterFormPlaceholder.password}
+            />
+
+            <Button type="submit">Sign up</Button>
+          </Stack>
+        </form>
+      </Stack>
+    </Center>
   );
 }
