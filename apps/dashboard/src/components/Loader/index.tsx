cedf035786efe235d1e82913d217df680d24807a
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-28 18:32:38 +0530

    login register fixed

--- apps/dashboard/src/components/Loader/index.tsx
+++ apps/dashboard/src/components/Loader/index.tsx
@@ -1,9 +1,9 @@
-import { Center } from "@mantine/core";
+import { Center, LoadingOverlay } from "@mantine/core";
 
 export function Loader({ isVisible = true }: { isVisible?: boolean }) {
   return (
     <Center h={"100vh"}>
-      <Loader isVisible={isVisible} />
+      <LoadingOverlay visible={isVisible} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
     </Center>
   );
 }
