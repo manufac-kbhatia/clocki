9dcfe99f624d66c4b5562dacda91c22474c8eabd
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-28 13:48:46 +0530

    minor

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Red_Hat_Display } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
 
 const redHatDisplay = Red_Hat_Display();
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
 import {
-  IconArrowRight,
   IconBeach,
   IconCalendarClock,
   IconClock,
