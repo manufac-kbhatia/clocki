47d361b428f154d923a23c2b80526eea45c76346
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-29 13:49:35 +0530

    next font issue fixed

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,7 @@ import { Red_Hat_Display } from "next/font/google";
 
 import "./globals.css";
 
-const redHatDisplay = Red_Hat_Display();
+const redHatDisplay = Red_Hat_Display({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Cloki: Simplify time tracking",
