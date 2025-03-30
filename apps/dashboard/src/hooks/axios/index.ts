f745c39553a7d376347cf10c587858ac3bc69c90
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-30 21:29:19 +0530

    react ci fix

--- apps/dashboard/src/hooks/axios/index.ts
+++ apps/dashboard/src/hooks/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "13.60.172.68:3000/api/v1";
+export const BASE_URL = "http://ec2-13-60-172-68.eu-north-1.compute.amazonaws.com:3000/api/v1";
 
 export default axios.create({ baseURL: BASE_URL });
 export const axiosPrivate = axios.create({ baseURL: BASE_URL, withCredentials: true });
