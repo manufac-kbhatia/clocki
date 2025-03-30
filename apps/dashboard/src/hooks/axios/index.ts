3f1ced2edf0b9f5035c3071f48128d7629b76962
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-30 20:10:11 +0530

    minor

--- apps/dashboard/src/hooks/axios/index.ts
+++ apps/dashboard/src/hooks/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:8080/api/v1";
+export const BASE_URL = "13.60.172.68:3000/api/v1";
 
 export default axios.create({ baseURL: BASE_URL });
 export const axiosPrivate = axios.create({ baseURL: BASE_URL, withCredentials: true });
--- apps/dashboard/src/utils.ts
+++ apps/dashboard/src/utils.ts
@@ -1,5 +1,3 @@
-export const API_URL = "http://localhost:8080/api/v1";
-
 export interface LocatioState {
   from?: string;
 }
