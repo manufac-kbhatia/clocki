8c6034a9507cea65c555f8be92fb32c0be535bc5
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-30 09:46:04 +0530

    fix controller imports

--- apps/http-server/src/routes/timeSheetRoutes.ts
+++ apps/http-server/src/routes/timeSheetRoutes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import express from "express";
 import { isAuthenticated, isAuthorized } from "../middlewares";
-import * as timeSheetControllers from "../controllers/timeSheetControllers";
 import catchAsync from "../middlewares/catchAsync";
 import { Role } from "@repo/schemas";
+import { addTimeEntry, getMyTimeEntries, getTimeEntries } from "../controllers/timeSheetControllers";
 
 const router: Router = express.Router();
 
 router
   .route("/log-time")
-  .get(isAuthenticated, catchAsync(timeSheetControllers.getMyTimeEntries))
-  .post(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(timeSheetControllers.addTimeEntry));
+  .get(isAuthenticated, catchAsync(getMyTimeEntries))
+  .post(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(addTimeEntry));
 
 router
   .route("/time-logs")
-  .get(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(timeSheetControllers.getTimeEntries));
+  .get(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(getTimeEntries));
 
 export default router;
