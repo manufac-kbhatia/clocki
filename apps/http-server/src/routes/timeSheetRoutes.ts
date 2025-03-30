da2d2bfb6e39a35cb2bb8dcfef095c840c4f8554
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-30 09:56:19 +0530

    minor

--- apps/http-server/src/routes/timeSheetRoutes.ts
+++ apps/http-server/src/routes/timeSheetRoutes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import express from "express";
-import { isAuthenticated, isAuthorized } from "../middlewares";
-import catchAsync from "../middlewares/catchAsync";
-import { Role } from "@repo/schemas";
-import { addTimeEntry, getMyTimeEntries, getTimeEntries } from "../controllers/timeSheetControllers";
+// import { isAuthenticated, isAuthorized } from "../middlewares";
+// import catchAsync from "../middlewares/catchAsync";
+// import { Role } from "@repo/schemas";
+// import { addTimeEntry, getMyTimeEntries, getTimeEntries } from "../controllers/timeSheetControllers";
 
 const router: Router = express.Router();
 
-router
-  .route("/log-time")
-  .get(isAuthenticated, catchAsync(getMyTimeEntries))
-  .post(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(addTimeEntry));
+// router
+//   .route("/log-time")
+//   .get(isAuthenticated, catchAsync(getMyTimeEntries))
+//   .post(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(addTimeEntry));
 
-router
-  .route("/time-logs")
-  .get(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(getTimeEntries));
+// router
+//   .route("/time-logs")
+//   .get(isAuthenticated, isAuthorized([Role.Admin, Role.Manager]), catchAsync(getTimeEntries));
 
 export default router;
