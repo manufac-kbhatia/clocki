2bb16cf4f545bb15f580a378f277ae1c7f53f833
manufac-kbhatia <bhatiakunal3256@gmail.com>
2025-03-31 20:50:25 +0530

    minor

--- apps/dashboard/src/hooks/axios/index.ts
+++ apps/dashboard/src/hooks/axios/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_URL = "http://ec2-13-60-172-68.eu-north-1.compute.amazonaws.com:3000/api/v1";
+export const BASE_URL = "http://ec2-13-60-172-68.eu-north-1.compute.amazonaws.com:8080/api/v1";
 
 export default axios.create({ baseURL: BASE_URL });
 export const axiosPrivate = axios.create({ baseURL: BASE_URL, withCredentials: true });
